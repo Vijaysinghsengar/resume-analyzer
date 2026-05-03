bb98dde1b98da13c263da33a686e8660900c1027
Vijaysinghsengar <yashs4765@gmail.com>
2026-05-03 22:04:55 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,16 @@
-from datasets import companies, indore_companies
+companies = [
+    {"name": "TCS", "type": "Service", "skills": ["java", "sql", "communication"]},
+    {"name": "Infosys", "type": "Service", "skills": ["python", "sql", "communication"]},
+    {"name": "Wipro", "type": "Service", "skills": ["testing", "java", "communication"]},
+    {"name": "Google", "type": "Product", "skills": ["python", "dsa", "machine learning"]},
+    {"name": "Amazon", "type": "Product", "skills": ["java", "dsa", "aws"]}
+]
+
+indore_companies = [
+    {"name": "Impetus Technologies", "type": "Product", "skills": ["python", "data engineering"]},
+    {"name": "InfoBeans", "type": "Service", "skills": ["java", "react"]},
+    {"name": "Tavant", "type": "Product", "skills": ["machine learning", "python"]},
+    {"name": "Softude", "type": "Service", "skills": ["web development", "php"]}
+]
 
 all_companies = companies + indore_companies
